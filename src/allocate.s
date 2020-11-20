929500d4417a43d513cca961e89163d12ae66ca4
Tony Josi <tonyjosinew@gmail.com>
2020-11-20 23:13:59 +0530

    initial

---
+++ src/allocate.s
@@ -0,0 +1,13 @@
+
+.section .data
+
+    .globl HEAP_START_ADDR
+    .globl CURRENT_BREAK_ADDR
+
+    HEAP_START_ADDR:
+        .long 0
+
+    CURRENT_BREAK_ADDR:
+        .long 0
+
+
---
+++ src/linux_asm_defs.s
@@ -0,0 +1,10 @@
+.equ    SYSCALL_EXIT,       1
+.equ    SYSCALL_READ,       3
+.equ    SYSCALL_WRITE,      4
+.equ    SYSCALL_OPEN,       5
+.equ    SYSCALL_CLOSE,      6
+.equ    SYSCALL_BRK,        45
+
+.equ    LINUX_SYSCALL,      0x80
+.equ    EOF,                0
+
