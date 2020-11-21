9f3eba3568d9432eb2173fba5e31f69b770fa675
Tony Josi <tonyjosinew@gmail.com>
2020-11-22 01:22:57 +0530

    deallocate func

---
+++ src/deallocate.s
@@ -0,0 +1,22 @@
+.include "linux_asm_defs.s"
+.include "heap_memory_header_struct.s"
+
+.section .text
+
+    .globl deallocate
+    .type deallocate, @function
+
+    .equ STACK_MEM_SEGMENT, 8
+
+    deallocate:
+
+        pushl %ebp
+        movl %esp, %ebp
+
+        movl STACK_MEM_SEGMENT(%ebp), %eax
+        subl $HP_MEM_HEADER_SIZE, %eax
+
+        movl $UNALLOCATED, HP_MEM_HEADER_ALLOC_FLAG_OFFSET(%eax)
+
+        movl %ebp, %esp
+        popl %ebp
